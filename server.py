fd228d48efe697ae9a9a3dd10b10493942f53f93
Robert Godlewski <robtechg5@gmail.com>
2022-02-06 16:31:08 -0800

    Created a static looking forum.

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
 
 
 app = Flask(__name__)
@@ -7,6 +7,25 @@ app.secret_key = 'server_hello'
 
 @app.route('/')
 def counter(): return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['name'] = str(request.form['name'])
+    session['location'] = str(request.form['location'])
+    session['fav_lang'] = str(request.form['fav_lang'])
+    session['comment'] = str(request.form['comment'])
+    # print(f"session details are: {str(session)}")
+    return redirect('/result')
+
+@app.route('/result')
+def result(): return render_template("result.html")
+
+# This section is only used for debugging
+'''@app.route('/clear')
+def clear_session():
+    session.clear()
+    print(f"Cleared session: {str(session)}")
+    return redirect('/')'''
 
 
 if __name__ == "__main__": app.run(debug=True, port=8000)
